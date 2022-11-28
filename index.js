e45ede36e2ac48dc428de153371eaf998bc45610
Robert <robethuku11@gmail.com>
2022-11-28 21:25:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+// multiply
+let num1 = 31,
+    num2 = 2;
+
+
+let multiply = num1 * num2;
+
+//random
+
+
+let random = Math.floor(Math.random() * 5) + 1;
+
+//mod
+let num3 = 24,
+    num4 = 5;
+
+let mod = num3 % num4;
+
+// max
+let max = Math.max(4,6,7,8,20);
+
+
+
+  
